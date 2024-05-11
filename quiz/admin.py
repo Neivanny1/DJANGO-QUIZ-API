f100ba9891e874feafc54922482da70416eb8f90
Neivanny1 <nevileneivanny@gmail.com>
2024-05-12 02:05:58 +0300

    added models for quiz app

--- quiz/admin.py
+++ quiz/admin.py
@@ -1,3 +1,49 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+
+@admin.register(models.Category)
+class CatAdmin(admin.ModelAdmin):
+    list_display = [
+        'name',
+    ]
+
+    search_fields = [
+        'name',
+    ]
+
+@admin.register(models.Quizzes)
+class QuizAdmin(admin.ModelAdmin):
+    list_display = [
+        'id',
+        'title',
+    ]
+    search_fields = [
+        'title',
+    ]
+class AnswerInlineModel(admin.TabularInline):
+    model = models.Answer
+    fields = [
+        'answer_text', 
+        'is_right'
+        ]
+@admin.register(models.Question)
+class QuestionAdmin(admin.ModelAdmin):
+    fields = [
+        'title',
+        'quiz',
+        ]
+    list_display = [
+        'title', 
+        'quiz',
+        'date_updated'
+        ]
+    inlines = [
+        AnswerInlineModel, 
+        ]
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = [
+        'answer_text', 
+        'is_right', 
+        'question'
+        ]
