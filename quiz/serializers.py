00b7b1f3bfb90b91ab67641424d69b59d60ce8f1
Neivanny1 <nevileneivanny@gmail.com>
2024-05-12 13:43:15 +0300

    using serializers

--- quiz/serializers.py
+++ quiz/serializers.py
@@ -1,3 +1,4 @@
+
 from rest_framework import serializers
 from .models import Quizzes, Question, Answer
 
@@ -8,6 +9,39 @@ class QuizSerializer(serializers.ModelSerializer):
         model = Quizzes
         fields = [
             'title',
-            'category',
-            'question'
+        ]
+
+class AnswerSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        
+        model = Answer
+        fields = [
+            'id',
+            'answer_text',
+            'is_right',
+        ]
+
+class RandomQuestionSerializer(serializers.ModelSerializer):
+
+    answer = AnswerSerializer(many=True, read_only=True)
+
+    class Meta:
+    
+        model = Question
+        fields = [
+            'title','answer',
+        ]
+
+
+class QuestionSerializer(serializers.ModelSerializer):
+
+    answer = AnswerSerializer(many=True, read_only=True)
+    quiz = QuizSerializer(read_only=True)
+
+    class Meta:
+    
+        model = Question
+        fields = [
+            'quiz','title','answer',
         ]
